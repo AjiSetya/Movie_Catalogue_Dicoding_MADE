9258f7021f0e934bd82ba12e16983a046b97ade2
AjiSetya <setyaaji07@gmail.com>
2019-07-24 20:58:58 +0700

    localization in english and bahasa

--- app/src/main/java/com/blogspot/blogsetyaaji/moviecatalogue/Activity/MainActivity.java
+++ app/src/main/java/com/blogspot/blogsetyaaji/moviecatalogue/Activity/MainActivity.java
@@ -1,10 +1,14 @@
 package com.blogspot.blogsetyaaji.moviecatalogue.Activity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.blogspot.blogsetyaaji.moviecatalogue.Fragment.MovieShowFragment;
 import com.blogspot.blogsetyaaji.moviecatalogue.Fragment.TVShowFragment;
@@ -29,5 +33,21 @@ public class MainActivity extends AppCompatActivity {
 
         TabLayout tabs = findViewById(R.id.tabs);
         tabs.setupWithViewPager(viewPager);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.action_change_settings){
+            Intent mIntent = new Intent(Settings.ACTION_LOCALE_SETTINGS);
+            startActivity(mIntent);
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
